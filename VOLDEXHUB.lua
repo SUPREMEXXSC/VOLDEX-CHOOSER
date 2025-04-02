76987e4bb45dd2b77b87c95d4fd4fcff49c7ba39
THEREALVOLDEX <rizzysjgma@gmail.com>
2025-04-02 11:31:13 +0500

    Update and rename VOLDEXHUB to VOLDEXHUB.lua

---
+++ VOLDEXHUB.lua
@@ -0,0 +1,58 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/slf0Dev/Ocerium_Project/main/Library.lua"))()
+Window = Library.Main("VOLDEX GAME CHOOSER","LeftAlt") -- change "LeftAlt" to key that you want will hide gui
+--//tab
+local Tab = Window.NewTab("OUR GAMES")
+
+
+--//section
+local Section = Tab.NewSection("NOTE USE MOBILE KEYBOARD AND PRESS S TO HIDE UI")
+--// Button
+local Button = Section.NewButton("mobile keyboard",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+end)
+--// Button
+local Button = Section.NewButton("TSB",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SUPREMEXXSC/Dexvoltsb/refs/heads/main/voldextsb.lua",true))()
+end)
+--// Button
+local Button = Section.NewButton("RACE CLICKER",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SUPREMEXXSC/RACE-CLICKER-SCRIPT/refs/heads/main/NEST%20HUB/I%20GOT%20RAPED.",true))()
+end)
+--// Button
+local Button = Section.NewButton("TOILET TOWER DEFENSE",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SUPREMEXXSC/VOLDEX-HUB./refs/heads/main/ttd/BEST%20SCRIPT/I%20GOT%20RAPED%2C",true))()
+end)
+--// Button
+local Button = Section.NewButton("JJS (JUJUTSU SHENIGENS)",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SUPREMEXXSC/VOLDEX-HUB-JJS/refs/heads/main/0%20effort",true))()
+end)
+--// Button
+local Button = Section.NewButton("DEAD RAILS (NOT MUCH FEATURES CUZ THEY GOT PATCH",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SUPREMEXXSC/BEST-DEAD-RAILS-SCRIPT/refs/heads/main/VOLDEXHUB",true))()
+end)
+--// Button
+local Button = Section.NewButton("legends of speed",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SUPREMEXXSC/Wkekekk/refs/heads/main/Ool",true))()
+end)
+--// Button
+local Button = Section.NewButton("BREAK IN STORY 2",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SUPREMEXXSC/BREAK-2/refs/heads/main/Atulw",true))()
+end)
+--// Button
+local Button = Section.NewButton("FISCH",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SUPREMEXXSC/FISCH/refs/heads/main/Patches",true))()
+end)
+--// Button
+local Button = Section.NewButton("executor script",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SUPREMEXXSC/-VOLDEX-EXECUTOR/refs/heads/main/Tomato",true))()
+end)
+--//tab
+local Tab = Window.NewTab("CREDITS")
+
+
+--//section
+local Section = Tab.NewSection("CREDITS THEVOLDEX AND VEXON FOR HELPING WITH TSB")
+--// Button
+local Button = Section.NewButton("COPY DISCORD",function()
+setclipboard("https://discord.gg/https://discord.gg/kJXXtyycjC")
+end)
